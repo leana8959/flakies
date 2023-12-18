e508b4cc395a34eb48422b9e67fbcf3cf974178d
Léana 江 <leana.jiang@icloud.com>
2023-12-18 22:08:27 +0100

    fix: why3

--- why3.nix
+++ why3.nix
@@ -1,37 +1,31 @@
-{ pkgs ? import <nixpkgs> {
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    opam-nix.url = "github:tweag/opam-nix";
+  };
 
-    overlays = [
-      (final: prev: {
-        why3 = prev.why3.overrideAttrs (old: {
-          version = "1.6.0";
-        });
-        cvc4 = prev.cvc4.overrideAttrs (old: {
-          version = "1.8";
-        });
-      })
-    ];
-  }
+  outputs = { self, nixpkgs, flake-utils, opam-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        formatter = pkgs.nixfmt;
 
-}:
+        devShell = let
+          opam = opam-nix.legacyPackages.${system};
+          why3 = pkgs.why3.overrideAttrs (prev: {
+            version = "1.6.0";
+            configureFlags = prev.configureFlags ++ [ "--enable-ide" ];
+          });
+          cvc4 = pkgs.cvc4.overrideAttrs (_: { version = "1.8"; });
+        in pkgs.mkShell {
+          packages = with pkgs; [
+            why3
 
-let
-  opam-nix = (import
-    (pkgs.fetchFromGitHub {
-      owner = "tweag";
-      repo = "opam-nix";
-      rev = "73b179d9a5e4b9849e7b857eef9ad2a065e489be";
-      hash = "sha256-38RprSyq38aGCsniQQaoYYmITzbgq2wssDQtVqnHfWE=";
-    })).legacyPackages."${pkgs.system}";
-
-in
-  pkgs.mkShell {
-
-    packages = with pkgs; ([
-      why3
-      cvc4
-      z3_4_12
-
-      opam-nix.alt-ergo."2.4.1"
-    ]);
-
-  }
+            cvc4
+            z3_4_12
+            opam.alt-ergo."2.4.1"
+          ];
+        };
+      });
+}
