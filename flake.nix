3ee9e26a8c2699a5b434f8f38f294cd3d474d1b0
Léana 江 <leana.jiang@icloud.com>
2024-01-13 16:43:55 +0100

    add: vanilla

--- flake.nix
+++ flake.nix
@@ -16,6 +16,8 @@
       };
 
       typst.path = ./typst;
+
+      vanilla.path = ./vanilla;
     };
   };
 }
---
+++ vanilla/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = ''
+    A flake that has nothing in it... yet
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixunstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      unstable = import nixunstable {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        # put your packages here !
+        buildInputs = with pkgs; [
+        ];
+      };
+    });
+}
