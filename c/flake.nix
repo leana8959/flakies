7adbad0b12c845f7729423df85514009ca682472
Léana 江 <leana.jiang@icloud.com>
2024-01-23 19:12:16 +0100

    fix(c): clang can now find stdlib

--- c/flake.nix
+++ c/flake.nix
@@ -9,18 +9,29 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
+    ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          llvmPackages_16.clang
-          gnumake
-        ];
-      };
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        llvm = pkgs.llvmPackages_16;
+      in {
+        devShells.default = pkgs.mkShell rec {
+          CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+          nativeBuildInputs = [
+            pkgs.gnumake
+            pkgs.clang-tools # fix headers not found
+            pkgs.clang
+            # # debugger
+            # llvm.lldb
+            # pkgs.gdb
+          ];
+          buildInputs = [
+            llvm.libcxx # stdlib for cpp
+          ];
+        };
+      }
+    );
 }
