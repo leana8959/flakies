be0b853e91d1e0dc8ad883d869a0615db1f2f1fc
Léana 江 <leana.jiang@icloud.com>
2024-01-31 19:13:55 +0100

    ported official rust template

---
+++ rust/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk-lib = pkgs.callPackage naersk {};
+    in {
+      defaultPackage = naersk-lib.buildPackage ./.;
+      formatter = pkgs.alejandra;
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            cargo
+            rustc
+            rustfmt
+            pre-commit
+            rustPackages.clippy
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+    });
+}
