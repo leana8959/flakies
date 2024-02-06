e02864ed9348ee2f2d5b11c9f88898efac762f05
Léana 江 <leana.jiang@icloud.com>
2024-02-06 18:22:08 +0100

    ref(why3): no need to pin why3

--- why3/flake.nix
+++ why3/flake.nix
@@ -1,21 +1,25 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     alt-ergo-pin.url = "github:NixOS/nixpkgs/1b95daa381fa4a0963217a5d386433c20008208a";
-    why3-pin.url = "github:NixOS/nixpkgs/e12230e71628a460bf82a8477242280868efe799";
   };
 
   outputs = {
     self,
     nixpkgs,
     alt-ergo-pin,
-    why3-pin,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            why3 = prev.why3.override {version = "1.6.0";};
+          })
+        ];
+      };
 
       alt-ergo =
         (import alt-ergo-pin {
@@ -23,26 +27,17 @@
           config.allowUnfree = true;
         })
         .alt-ergo;
-
-      why3 =
-        (import why3-pin {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              why3 = prev.why3.overrideAttrs (old: {configureFlags = (old.configureFlags or []) ++ ["--enable-ide"];});
-            })
-          ];
-        })
-        .why3;
     in {
       formatter = pkgs.alejandra;
       devShell = pkgs.mkShell {
         packages = [
+          pkgs.why3
+          pkgs.zip
+
+          # Solvers
           alt-ergo
-          why3
           pkgs.cvc4
           pkgs.z3_4_12
-          pkgs.zip
         ];
       };
     });
