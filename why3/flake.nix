01f54a53350396c9f30828f9ef29d8dea24ceef5
Léana 江 <leana.jiang@icloud.com>
2024-02-18 17:04:03 +0000

    Detect why3 config at build time (#1)
    Reviewed-on: https://git.earth2077.fr/leana/flakies/pulls/1
    Co-authored-by: Léana 江 <leana.jiang@icloud.com>
    Co-committed-by: Léana 江 <leana.jiang@icloud.com>

--- why3/flake.nix
+++ why3/flake.nix
@@ -34,17 +34,30 @@
           config.allowUnfree = true;
         })
         .alt-ergo;
+
+      provers = [
+        alt-ergo
+        pkgs.cvc4
+        pkgs.z3_4_12
+      ];
+
+      why3-wrapped = pkgs.symlinkJoin {
+        name = "why3";
+        # Generate configuration in the store, and wrap why3 with the corresponding option
+        paths = [pkgs.why3];
+        buildInputs = provers;
+        nativeBuildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          $out/bin/why3 config detect --config=$out/why3.conf
+          wrapProgram $out/bin/why3 --add-flags "--config=$out/why3.conf"
+        '';
+      };
     in {
       formatter = pkgs.alejandra;
       devShell = pkgs.mkShell {
         packages = [
-          pkgs.why3
-          pkgs.zip
-
-          # Solvers
-          alt-ergo
-          pkgs.cvc4
-          pkgs.z3_4_12
+          pkgs.zip # used for exporting session
+          why3-wrapped
         ];
       };
     });
