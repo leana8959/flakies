49af7289b85f880d12655f9ffb2bedb91ee3b47d
Léana 江 <leana.jiang@icloud.com>
2024-02-27 19:12:35 +0100

    fix(haskell): packages weren't in scope for shell

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -40,11 +40,7 @@
         buildInputs = [pkgs.makeWrapper];
         postBuild = ''
           wrapProgram $out/bin/stack \
-            --add-flags "\
-              --no-nix \
-              --system-ghc \
-              --no-install-ghc \
-            "
+            --add-flags "--no-nix --system-ghc --no-install-ghc"
         '';
       };
     in rec {
@@ -53,7 +49,7 @@
       packages.default = pkgs.haskellPackages.callCabal2nix "simple" ./. {};
 
       devShells.default = packages.default.env.overrideAttrs {
-        packages = devTools;
+        buildInputs = devTools;
 
         # Make external Nix c libraries like zlib known to GHC, like pkgs.haskell.lib.buildStackProject does
         # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
