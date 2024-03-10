74d6c308ab6581a6b4e2e0f26a5cc87d9ee364af
Léana 江 <leana.jiang@icloud.com>
2024-03-10 12:47:13 +0100

    fix(haskell): use mkShell instead

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -22,16 +22,14 @@
 
       devTools = [
         stack-wrapped
-        hPkgs.ghc # GHC compiler in the desired version (will be available on PATH)
-        hPkgs.stylish-haskell # Haskell formatter
-        hPkgs.hoogle # Lookup Haskell documentation
-        hPkgs.haskell-language-server # LSP server for editor
-        hPkgs.cabal-install
+        hPkgs.ghc
+        hPkgs.haskell-language-server
+        hPkgs.stylish-haskell
       ];
 
       # Wrap Stack to work with our Nix integration. We don't want to modify
       # stack.yaml so non-Nix users don't notice anything.
-      # -no-nix: We don't want Stack's way of integrating Nix.
+      # --no-nix: We don't want Stack's way of integrating Nix.
       # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
       # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
       stack-wrapped = pkgs.symlinkJoin {
@@ -43,12 +41,12 @@
             --add-flags "--no-nix --system-ghc --no-install-ghc"
         '';
       };
-    in rec {
+    in {
       formatter = pkgs.alejandra;
 
       packages.default = pkgs.haskellPackages.callCabal2nix "simple" ./. {};
 
-      devShells.default = packages.default.env.overrideAttrs {
+      devShells.default = pkgs.mkShell {
         buildInputs = devTools;
 
         # Make external Nix c libraries like zlib known to GHC, like pkgs.haskell.lib.buildStackProject does
